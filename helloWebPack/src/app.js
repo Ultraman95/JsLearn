ad1f2ffad6c8d87e483f923fc149d0202d9a6942
shilf <tcl.shilf@gmail.com>
2019-10-18 21:40:01 +0800

    自动生成html文件

--- helloWebPack/src/app.js
+++ helloWebPack/src/app.js
@@ -1,50 +1,12 @@
-//import '@babel/polyfill'  // 全局引入babel/polyfill，会导致产生的包过大
-
-/* 同步代码
-import _ from 'lodash'
-console.log(_.join(['a', 'b', 'c'], '***'))
-*/
-
-// 异步代码
-// import(/* webpackChunkName: 'a'*/ './a').then(function(a) {
-//   console.log(a)
-// })
-
-// import(/* webpackChunkName: 'b'*/ './b').then(function(b) {
-//   console.log(b)
-// })
-
 
 document.addEventListener('click', function() {
-  import(/* webpackChunkName: 'use-lodash'*/ 'lodash').then(function(_) {
-    console.log(_.join(['3', '4']))
-  })
+  // import(/* webpackChunkName: 'use-lodash'*/ 'lodash').then(function(_) {
+  //   console.log(_.join(['9', '11']))
+  // })
+
+  const element = document.createElement('div')
+  element.innerHTML = 'Hello World'
+  document.body.appendChild(element)
 })
 
 console.log('hello webPack!')
-
-
-// webpack 支持 ES6、CommonJs 和 AMD 规范，测试 ES6 语法是否通过 babel 转译
-const array = [1, 2, 3]
-const isES6 = () => console.log(...array)
-isES6()
-const arr = [new Promise(() => {}), new Promise(() => {})]
-arr.map(item => {
-  console.log(item)
-})
-
-// ES6
-import sum from './vendor/sum'
-console.log('sum(1, 2) = ', sum(1, 2))
-
-// CommonJs
-var minus = require('./vendor/minus')
-console.log('minus(1, 2) = ', minus(1, 2))
-
-//在实际写代码的时候，最好使用 ES6 和 CommonJS 的规范来写
-// AMD
-/*
-require(['./vendor/multi'], function(multi) {
-  console.log('multi(1, 2) = ', multi(1, 2))
-})*/
-
--- helloWebPack/webpack.config.js
+++ helloWebPack/webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 //const CleanWebpackPlugin = require('clean-webpack-plugin')    //clean-webpack-plugin老版本 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')  // clean-webpack-plugin 3.0以上的版本
 
+const HtmlWebpackPlugin = require('html-webpack-plugin') // 引入插件
+
 // path.resolve() 方法会把一个路径或路径片段的序列解析为一个绝对路径
 // __dirname: 当前模块的文件夹名称
 console.log('__dirname: ', __dirname)
@@ -13,7 +15,8 @@ module.exports = {
     app: './src/app.js' // 需要打包的文件入口
   },
   output: {
-    publicPath: __dirname + '/dist/', // js 引用的路径或者 CDN 地址
+    //publicPath: __dirname + '/dist/', // js 引用的路径或者 CDN 地址
+    publicPath: './',  //绝对路径
     path: path.resolve(__dirname, 'dist'), // 打包文件的输出目录
     filename: '[name].bundle.js', // 打包后生产的 js 文件
     chunkFilename: '[name].js' // 代码分割后的文件名
@@ -29,22 +32,16 @@ module.exports = {
       automaticNameDelimiter: '~',
       name: true,
       cacheGroups: {
-        lodash: { //单独打包第三方的库
+        lodash: {   //单独打包第三方的库，这个第三方库会单独存在一个文件，这个文件不能被删除，因为会被vendors调用
           name: 'lodash',
           test: /[\\/]node_modules[\\/]lodash[\\/]/,
-          priority: 10  // 优先级要大于 vendors 不然会被打包进 vendors
-        },
-        commons: {
-          name: 'commons',
-          minSize: 0, //表示在压缩前的最小模块大小,默认值是 30kb
-          minChunks: 2, // 最小公用次数
-          priority: 5, // 优先级
-          reuseExistingChunk: true // 公共模块必开启
+          priority: -5    //优先级要大于 vendors 不然会被打包进 vendors
         },
         vendors: {
           name: 'vendors',
-          test: /[\\/]node_modules[\\/]/,   //正则过滤，只有node_modules引入的第三方库才会被分割
-          priority: -10   //优先级高的会被先打包
+          test: /[\\/]node_modules[\\/]/,   //正则过滤，只有node_modules引入的第三方库才会被打包到vendors，除非单独打包第三方库。
+                                            //所以打包的原则主页面直接渲染需要的三方库，打包到vendors中，其他间接使用的三方库单独打包
+          priority: -10     //优先级高的会被先打包
         },
         default: {
           minChunks: 2,
@@ -56,7 +53,20 @@ module.exports = {
   },
   plugins: [
     // 默认情况下，此插件将删除 webpack output.path目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产。
-    new CleanWebpackPlugin() 
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      // 打包输出HTML
+      title: '自动生成HTML',
+      minify: {
+        // 压缩 HTML 文件
+        removeComments: true, // 移除 HTML 中的注释
+        collapseWhitespace: true, // 删除空白符与换行符
+        minifyCSS: true // 压缩内联 css
+      },
+      filename: 'index.html', // 生成后的文件名
+      template: 'index.html', // 根据此模版生成 HTML 文件
+      chunks: ['app'] // entry中的 page 入口才会被打包
+    })
   ],
   module: {
     rules: [
